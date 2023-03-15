bb22d113126b89fcd5826fd913192559109370bd
duongtt1 <trinhtungduong2019@gmail.com>
2023-03-15 11:37:44 +0700

    MD3_Lesson9_Practice2

---
+++ src/rikkei/academy/AbsoluteNumberCalculator.java
@@ -0,0 +1,12 @@
+package rikkei.academy;
+
+public class AbsoluteNumberCalculator {
+    public static int findAbsolute(int number){
+//        throw new UnsupportedOperationException();
+//        return 0;
+        if(number > 0)
+            return -number;
+        return number;
+    }
+
+}
---
+++ src/rikkei/academy/Demo.java
@@ -0,0 +1,22 @@
+//package rikkei.academy;
+//
+//import java.util.Scanner;
+//
+//public class Demo {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int num = 0;
+//        int sumEven = 0;
+//        System.out.println("Nhập vào độ dài dãy số bất kì: ");
+//
+//        do {
+//            num = scanner.nextInt();
+//        } while (num < 0);
+//        for (int i = 0; i < num; i++) {
+//            if (i % 2 != 0){
+//                sumEven += i;
+//            }
+//        }
+//        System.out.println("Tổng của các số lẻ là: " +sumEven);
+//    }
+//}
